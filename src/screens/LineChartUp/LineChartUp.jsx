26f341c4d0d53bd3c31d422a311a9dd34fdc584f
pawelsloboda5 <128259232+pawelsloboda5@users.noreply.github.com>
2023-08-03 14:06:17 -0400

    Initial commit

---
+++ src/screens/LineChartUp/LineChartUp.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./style.css";
+
+export const LineChartUp = () => {
+  return <div className="line-chart-up">Sign Up</div>;
+};
---
+++ src/screens/LineChartUp/index.js
@@ -0,0 +1,1 @@
+export { LineChartUp } from "./LineChartUp";
